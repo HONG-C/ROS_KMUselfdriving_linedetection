2e0ef684befd51fe535a0acd78b8cfa30921391e
HONG-C <jihun3900@naver.com>
2021-05-12 14:46:22 +0900

    roi,가우시안 필터, hough 변환 등을 추가한 차선인식 완료

--- line_drive.py
+++ line_drive.py
@@ -18,6 +18,38 @@ def canny(img, low_threshold, high_threshold): # Canny 알고리즘
 def gaussian_blur(img, kernel_size): # 가우시안 필터
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
+def region_of_interest(img, vertices, color3=(255,255,255), color1=255): # ROI 셋팅
+
+    mask = np.zeros_like(img) # mask = img와 같은 크기의 빈 이미지
+    
+    if len(img.shape) > 2: # Color 이미지(3채널)라면 :
+        color = color3
+    else: # 흑백 이미지(1채널)라면 :
+        color = color1
+        
+    # vertices에 정한 점들로 이뤄진 다각형부분(ROI 설정부분)을 color로 채움 
+    cv2.fillPoly(mask, vertices, color)
+    
+    # 이미지와 color로 채워진 ROI를 합침
+    ROI_image = cv2.bitwise_and(img, mask)
+    return ROI_image
+
+def draw_lines(img, lines, color=[0, 0, 255], thickness=2): # 선 그리기
+    if lines is not None:
+	    for line in lines:
+		for x1,y1,x2,y2 in line:
+		    cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap): # 허프 변환
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+
+    return line_img
+
+def weighted_img(img, initial_img, a=1, b=1.0, c=0.0): # 두 이미지 operlap 하기
+    return cv2.addWeighted(initial_img, a, img, b, c)
+
 # draw rectangle
 def draw_rectangle(img, lpos, rpos, offset=0):
     center = (lpos + rpos) / 2
@@ -73,17 +105,19 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         ret, image = cap.read()
         pos, frame = process_image(image)
-        height, width = image.shape[:2] # 이미지 높이, 너비
-
+	height, width = image.shape[:2] # 이미지 높이, 너비
 	gray_img = grayscale(image) # 흑백이미지로 변환
-    
 	blur_img = gaussian_blur(gray_img, 3) # Blur 효과
-        
 	canny_img = canny(blur_img, 70, 210) # Canny edge 알고리즘
-
-	cv2.imshow('result',canny_img) # Canny 이미지 출력
+	vertices = np.array([[(50,height),(width/2-45, height/2+60), (width/2+45, height/2+60), (width-50,height)]], dtype=np.int32)
+	ROI_img = region_of_interest(canny_img, vertices) # ROI 설정
+	hough_img = hough_lines(ROI_img, 1, 1 * np.pi/180, 30, 10, 20) # 허프 변환
+	result = weighted_img(hough_img, image) # 원본 이미지에 검출된 선 overlap
+	cv2.imshow('result',result) # 결과 이미지 출력
+	cv2.imshow('ROI_img',ROI_img) # roi 이미지 출력        
         steer_angle = 0
         draw_steer(frame, steer_angle)
 
         if cv2.waitKey(3) & 0xFF == ord('q'):
             break
+
